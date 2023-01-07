4ad665124d9c1e0ea6188eb2419ee2fb0e436290
Antoine Marteau <antoine.marteau@protonmail.com>
2023-01-07 16:49:03 +0100

    remove lua lsp warning

--- home/neovim/nvim/lua/lsp.lua
+++ home/neovim/nvim/lua/lsp.lua
@@ -66,6 +66,8 @@ require 'lspconfig'.sumneko_lua.setup {
             workspace = {
                 -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file("", true),
+                -- Prevent warning
+                checkThirdParty = false,
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
