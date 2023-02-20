4cc188fb87cc636bd29789e10abc79ec84bcc7cf
Antoine Marteau <antoine.marteau@protonmail.com>
2023-02-20 13:50:01 +0100

    add mpi and singularity getdp shortcut

--- home/modules/common/thesis.nix
+++ home/modules/common/thesis.nix
@@ -1,9 +1,31 @@
 { config, pkgs, ... }:
 {
-    home.packages = with pkgs; [
-        gmsh
-        #singularity
-    ];
+    home = {
+        packages = with pkgs; [
+            gmsh
+            mpi
+        ];
 
-    #programs.singularity.enable = true;
+        sessionPath = [ "$HOME/.local/bin" ]; #add it to system path
+        file = {
+            sgetdp = {
+                executable = true;
+                text = ''
+                    #!/bin/sh
+
+                    # Absolute path to container
+                    CONTAINER=$HOME/prog/these/gricad/singularity_container.sif
+
+                    CONT_SOURCE_DIR=/home/working_dir/
+                    singularity exec \
+                        --bind $PWD:$CONT_SOURCE_DIR \
+                        --home $CONT_SOURCE_DIR \
+                        $CONTAINER \
+                        getdp $@
+                '';
+
+                target = ".local/bin/sgetdp";
+            };
+        };
+    };
 }
