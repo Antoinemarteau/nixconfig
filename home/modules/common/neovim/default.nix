cf8d51f37484de33e2dc25583f64fcd89a9aa16f
Antoine Marteau <antoine.marteau@protonmail.com>
2023-06-05 08:41:14 +0200

    add latex ftplugin

--- home/modules/common/neovim/default.nix
+++ home/modules/common/neovim/default.nix
@@ -9,6 +9,7 @@
     ];
 
     xdg.configFile."nvim/UltiSnips".source = ./nvim/UltiSnips;
+    xdg.configFile."nvim/ftplugin".source = ./nvim/ftplugin;
 
     programs.nixvim = {
         enable = true;
