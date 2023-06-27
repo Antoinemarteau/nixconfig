1147dd4388625a1f4f2c8a826bed2690a2faa438
Antoine Marteau <antoine.marteau@protonmail.com>
2023-06-27 14:21:40 +0200

    fix command mappings

---
+++ home/modules/common/neovim/command_keymappings.nix
@@ -0,0 +1,22 @@
+{ config, ... }:
+{
+    programs.nixvim = {
+
+        maps = config.nixvim.helpers.mkMaps {silent = false;} {
+          # Navigation & completion in command mode
+          command."<A-v>" = "<C-B>";                          # Go to begining of line
+          command."<A-d>" = "<S-Left>";                       # Cursor one word left
+          command."<A-l>" = "<S-Right>";                      # Cursor one word right
+          command."<A-j>" = "<End>";                          # Go to end of line
+
+          command."<A-c>" = "<Up>";    # Go to parent folder  | back history command
+          command."<A-r>" = "<Down>";  # Enter file/folder    | forward history command
+
+          command."<A-\">"= "<C-W>";                          # Delete word before cursor
+          command."<A-q>" = "<C-U>";                          # Delete all char before cursor
+          command."<A-g>" = "<BS>";                           # Delete char before cursor
+          command."<A-h>" = "<Del>";                          # Delete char after cursor
+        };
+    };
+}
+
--- home/modules/common/neovim/keymappings.nix
+++ home/modules/common/neovim/keymappings.nix
@@ -1,5 +1,8 @@
 { config, ... }:
 {
+    # command mode keymaps
+    imports = [./command_keymappings.nix];
+
     # Source : https://idie.ru/posts/vim-modern-cpp/#enhancing-syntax-highlighting-for-modern-c
     programs.nixvim = {
         globals = {
@@ -106,22 +109,8 @@
           # move selected line / block of text in visual mode
           visual."S" = ":m '<-2<CR>gv=gv";
           visual."T" = ":m '>+1<CR>gv=gv";
-        };
 
-        maps = config.nixvim.helpers.mkMaps {silent = false;} {
-          # Navigation & completion in command mode
-          command."<A-v>" = "<C-B>";                          # Go to begining of line
-          command."<A-d>" = "<S-Left>";                       # Cursor one word left
-          command."<A-l>" = "<S-Right>";                      # Cursor one word right
-          command."<A-j>" = "<End>";                          # Go to end of line
-
-          command."<A-c>" = "<Up>";    # Go to parent folder  | back history command
-          command."<A-r>" = "<Down>";  # Enter file/folder    | forward history command
-
-          command."<A-\">"= "<C-W>";                          # Delete word before cursor
-          command."<A-q>" = "<C-U>";                          # Delete all char before cursor
-          command."<A-g>" = "<BS>";                           # Delete char before cursor
-          command."<A-h>" = "<Del>";                          # Delete char after cursor
+          # command mappings in other file
         };
 
         extraConfigLua = ''
