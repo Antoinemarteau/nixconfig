ce09e7d7df79287cad18c6a5b3faf7505f96256e
Antoine Marteau <antoine.marteau@protonmail.com>
2023-06-27 14:06:10 +0200

    add other buffers' text as completion source

--- home/modules/common/neovim/plugins/nvim-cmp.nix
+++ home/modules/common/neovim/plugins/nvim-cmp.nix
@@ -43,7 +43,25 @@
           {name = "path";}
           {name = "nvim_lsp";}
           {name = "ultisnips";}
-          {name = "buffer";}
+          {
+            name = "buffer";
+            option = { # all buffers
+              get_bufnrs.__raw = ''function()
+                    return vim.api.nvim_list_bufs()
+                  end
+              '';
+            };
+            #option = { # all visible buffers
+            #  get_bufnrs.__raw = ''function()
+            #    local bufs = {}
+            #    for _, win in ipairs(vim.api.nvim_list_wins()) do
+            #      bufs[vim.api.nvim_win_get_buf(win)] = true
+            #    end
+            #    return vim.tbl_keys(bufs)
+            #  end
+            #'';
+            #};
+          }
           #{name = "cmp_tabnine";}
         ];
       };
