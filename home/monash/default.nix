747c86b60e6ab78312b57f49a2520a94d7ed7249
Antoine Marteau <antoine.marteau@protonmail.com>
2024-06-13 10:12:45 +1000

    fix indent

--- home/monash/default.nix
+++ home/monash/default.nix
@@ -32,7 +32,7 @@
           math_office_output = pkgs.writeScript "math_office" ''
           #!/bin/sh
           xrandr --output eDP-1 --off --output DP-1 --off --output HDMI-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-4-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-4-2 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-4-3 --off
-      '';
+          '';
       in {
           "${mod}+m" = "exec --no-startup-id ${math_office_output}";
       };
