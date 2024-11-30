66ccf7f9c75e2494ed8883431999dabc6edcc994
Antoine Marteau <antoine.marteau@protonmail.com>
2024-11-30 21:10:17 +1100

    ankama-launcher

---
+++ home/modules/ankama.nix
@@ -0,0 +1,21 @@
+{ lib, pkgs, appimageTools, fetchurl, ...}: #with lib;
+let
+  name = "ankama-launcher";
+  src = fetchurl {
+    url = "https://launcher.cdn.ankama.com/installers/production/Dofus-Setup-x86_64.AppImage";
+    hash = "sha256-SfK8zfWcxpILi6VGK72ja0WgIDIvZ4nOR70ToEt9rMk="; # Change for the sha256 you get after running nix-prefetch-url https://download.ankama.com/launcher/full/linux
+    name = "ankama-launcher.AppImage";
+ };
+
+  appimageContents = appimageTools.extractType2 { inherit name src; };
+in
+  appimageTools.wrapType2 {
+    inherit name src;
+
+    extraInstallCommands = ''
+      install -m 444 -D ${appimageContents}/zaap.desktop $out/share/applications/ankama-launcher.desktop
+      sed -i 's/.*Exec.*/Exec=ankama-launcher/' $out/share/applications/ankama-launcher.desktop
+      install -m 444 -D ${appimageContents}/zaap.png $out/share/icons/hicolor/256x256/apps/zaap.png
+    '';
+}
+
--- home/modules/games.nix
+++ home/modules/games.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, fetchurl, ... }:
 {
     home.packages = with pkgs; [
         prismlauncher
         #minecraft TODO test if it is fixed
         lutris
         wine
+        (callPackage ./ankama.nix {})
     ];
 }
