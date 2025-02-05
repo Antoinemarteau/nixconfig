076e03a698532aaad5f82c526000e9f494fa3849
Antoine Marteau <antoine.marteau@protonmail.com>
2025-02-05 12:39:28 +1100

    add Pigments.py for Documenter pdf export

--- home/modules/common/latex.nix
+++ home/modules/common/latex.nix
@@ -4,6 +4,7 @@
         texlive.combined.scheme-full
         inkscape # for svg support
         liberation-sans-narrow # for phd thesis frontpage
+        python312Packages.pygments # for minted latex package, for Documenter.jl latex export
     ];
 
     # Palatino font for plots in julia for latex document
