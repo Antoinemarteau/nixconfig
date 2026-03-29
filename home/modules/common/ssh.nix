d0ab90708ea1b0124f21bc2707c48f9ee37008e5
Antoine Marteau <antoine.marteau@protonmail.com>
2026-03-30 10:38:18 +1100

    fix new ssh config with default disabled

--- home/modules/common/ssh.nix
+++ home/modules/common/ssh.nix
@@ -4,6 +4,20 @@
         enableDefaultConfig = false; # unsure if that's breaking stuff
 
         matchBlocks = {
+            # replacement of disabled (and deprecated) default config above
+            "*" = {
+                forwardAgent = false;
+                addKeysToAgent = "no";
+                compression = false;
+                serverAliveInterval = 0;
+                serverAliveCountMax = 3;
+                hashKnownHosts = false;
+                userKnownHostsFile = "~/.ssh/known_hosts";
+                controlMaster = "no";
+                controlPath = "~/.ssh/master-%r@%n:%p";
+                controlPersist = "no";
+            };
+
             ##########
             # Monash #
             ##########
